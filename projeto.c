86a85731559907c316fc5b5ec651838e605a5450
AnaSPires <42739437+AnaSPires@users.noreply.github.com>
2019-10-19 23:35:16 -0300

    Inicio do infeeeeeeerno

---
+++ projeto.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void lerArquivo(char*arq){
+    FILE* file;
+    file = fopen(arq,"r");
+    //como ler a primeira linha?
+}
+void main(){
+    char * arquivo;
+    printf('Digite o nome do arquivo desejado');
+    scanf("%s", arquivo);
+    lerArquivo(arquivo);
+}
+
+
