00f6fd5c690e72b65ace91bad52d6230a8fc79cd
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-10-27 15:25:39 -0300

    a

--- Compactador/main.c
+++ Compactador/main.c
@@ -8,68 +8,68 @@ int main()
 {
    FILE *ponteiroArquivo;
    NoFila* f = NULL;
-  char arq[30];
+   char arq[30];
 
-  printf("Opcao escolhida: ");
+   printf("Opcao escolhida: ");
 
-  scanf("%s", arq);
+   scanf("%s", arq);
 
-  ponteiroArquivo = fopen(arq, "r");
+   ponteiroArquivo = fopen(arq, "r");
 
-  if(ponteiroArquivo == NULL)
-     printf("Erro na abertura do arquivo!");
+   if(ponteiroArquivo == NULL)
+      printf("Erro na abertura do arquivo!");
 
-  fseek(ponteiroArquivo, 0, SEEK_END);
-  tamanho = ftell(ponteiroArquivo);
+   fseek(ponteiroArquivo, 0, SEEK_END);
+   tamanho = ftell(ponteiroArquivo);
 
-  int qtd = 0;
-  rewind(ponteiroArquivo);
+   int qtd = 0;
+   rewind(ponteiroArquivo);
 
-  char c = getc(ponteiroArquivo);
-  while(c != EOF)
-    {
-        NoFila* atual = buscar(c, f);
-        f=atual;
-        c = getc(ponteiroArquivo);
-      }
+   char c = getc(ponteiroArquivo);
+   while(c != EOF)
+   {
+      NoFila* atual = buscar(c, f);
+      f=atual;
+      c = getc(ponteiroArquivo);
+   }
 
-       char car;
-         int fre;
+   char car;
+   int fre;
 
-          NoFila* teste = f;
-          while(teste->prox !=NULL)
-          {
-              car = teste->info->caracter;
-              fre = teste->info->frequencia;
-              //printf("%c",car);
-             // printf("%i", fre);
-             // printf("\n");
-              teste = teste->prox;
-              qtd++;
-          }
-              car = teste->info->caracter;
-              fre = teste->info->frequencia;
-             // printf("%c",car);
-             // printf("%i", fre);
-             // printf("\n\n");
-              qtd++;
+   NoFila* teste = f;
+   while(teste->prox !=NULL)
+   {
+        car = teste->info->caracter;
+        fre = teste->info->frequencia;
+        //printf("%c",car);
+        // printf("%i", fre);
+        // printf("\n");
+        teste = teste->prox;
+        qtd++;
+   }
+   car = teste->info->caracter;
+   fre = teste->info->frequencia;
+   // printf("%c",car);
+   // printf("%i", fre);
+   // printf("\n\n");
+   qtd++;
 
+   while(qtd > 1)
+   {
+      NoArvore* primeiro = pop(&f);
+      printf("%c",primeiro->caracter);
+      printf("%i",primeiro->frequencia);
 
-    while(qtd > 1)
-    {
-        NoArvore* primeiro = pop(&f);
-        printf("%c",primeiro->caracter);
-        printf("%i",primeiro->frequencia);
-
-        NoArvore *segundo = pop(&f);
-        //printf("%c",segundo->caracter);
-        //printf("%i",segundo->frequencia);
+      NoArvore *segundo = pop(&f);
+      //printf("%c",segundo->caracter);
+      //printf("%i",segundo->frequencia);
 
 
         int frequencia = (primeiro->frequencia) + (segundo->frequencia);
         NoFila* novo = (NoFila*)malloc(sizeof(NoFila));
         novo->info->frequencia = frequencia;
         novo->info->vazio = 1;
+
 
         //printf("%i", novo->info->frequencia);
         //printf("\n");
@@ -85,7 +85,37 @@ int main()
 
         f = insira(f,novo);
     }
-    NoFila* filaLet = (*NoFila)malloc(sizeof(NoFila));
-    int cod;
-    while()
+    //NoFila* filaLet = (NoFila*)malloc(sizeof(NoFila));
+    //int cod;
+    //while()
+
+    NoFila* aux = (NoFila*)malloc(sizeof(Nofila));
+    NoArvore* auxArvore = (NoArvore*)malloc(sizeof(NoArvore));
+    int cod[8]; //declarar dinamicamente
+    int pos = 0;
+
+    while(aux->info->esq != NULL && aux->info->dir != NULL)
+    {
+        aux = aux->prox;
+
+        printf("%c", aux->info->caracter);
+
+        if(aux->info->esq == NULL)
+        {
+            cod[pos++] = "0";
+
+        }
+
+        if(aux->info->dir == NULL)
+        {
+            cod[pos++] = "1";
+
+        }
+    }
+
+    Letra* letra = create();
+    letra->caracter = aux->caracter;
+
+
+
   }
